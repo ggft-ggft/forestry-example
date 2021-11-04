3b7071b34532e356713f5a08b78c376dc994323f
De Ioanni, Giuliano <giuliano.deioanni@gft.com>
2021-11-04 20:24:58 +0100

    loololo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("uploads");
+  eleventyConfig.addPassthroughCopy("admin/config.yml");
+
     return {
       pathPrefix: "/forestry-example/"
     }
--- boh.md
+++ boh.md
@@ -6,3 +6,5 @@ delle_scelte: ciao
 Pappero Pappa
 
 {{ ununmero }}
+
+![Immagine!]({{ '/uploads/93006970_2580424598912944_5169227414022127616_o.jpg' | url }})
